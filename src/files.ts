03f481b9e8a43cb5155cf558cbcf5d15346bb53e
Manav Notnani <manav.notnani@gmail.com>
2024-02-26 10:31:14 +0530

    changes made

--- src/files.ts
+++ src/files.ts
@@ -1,3 +1,18 @@
+import fs from "fs";
+import path from "path";
 export const getAllFiles = (folderPath: string) => {
+  let response: string[] = [];
+  const allFiles = fs.readdirSync(folderPath);
+  allFiles.forEach((file) => {
+    const fullFilePath = path.join(folderPath, file);
+    if (fs.statSync(fullFilePath).isDirectory()) {
+      response = response.concat(getAllFiles(fullFilePath));
+    } else {
+      response.push(fullFilePath);
+    }
+  });
+  console.log("response", response);
+  return response;
+};
 
-}
+getAllFiles(`./build/output/jj14k`);
